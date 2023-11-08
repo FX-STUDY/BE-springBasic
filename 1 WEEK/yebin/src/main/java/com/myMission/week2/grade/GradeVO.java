c2c66e1517765ecda5429ce3234b38a7f18a5eaf
02yebin <agabin0214@naver.com>
2023-11-08 16:27:33 +0900

    [BE] (FEAT) GradeVO 클래스 생성
    회원 등급에 관련하여 나중에 추가될 수 있으므로 클래스를 따로 빼놓았습니다.

---
+++ 1 WEEK/yebin/src/main/java/com/myMission/week2/grade/GradeVO.java
@@ -0,0 +1,12 @@
+package com.myMission.week2.grade;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// 회원 등급 관련
+@Getter
+@Setter
+public class GradeVO {
+    private String Basic;
+    private String VIP;
+}
