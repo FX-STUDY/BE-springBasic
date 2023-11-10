f8a393adb4becd85c0afb3bf8561bd97d682dfeb
02yebin <agabin0214@naver.com>
2023-11-11 00:10:45 +0900

    [BE] (REFACTOR) GradeVO 수정
    GradeVO 클래스를 enum 클래스로 수정

--- 1 WEEK/yebin/src/main/java/com/myMission/yebin/grade/GradeVO.java
+++ 1 WEEK/yebin/src/main/java/com/myMission/yebin/grade/GradeVO.java
@@ -4,11 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 // 회원 등급 관련
-@Getter
-@Setter
-public class GradeVO {
-    private String Basic;
-    private String VIP;
 
+public enum GradeVO {
+    Basic, VIP
 
 }
