e70cf8bc61413539f261679017fae0f21c79e888
haramm <100928807+haramm@users.noreply.github.com>
2023-11-12 14:49:53 +0900

    [BE] (feat) itemVO 구현
    주문할 상품의 정보(이름,가격)을 저장할 VO객체 생성

---
+++ 1 WEEK/haram_1week/src/main/java/com/example/Project_1week/item/ItemVO.java
@@ -0,0 +1,23 @@
+package com.example.Project_1week.item;
+
+public class ItemVO {
+
+    private String itemName;
+    private int itemPrice;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+}
