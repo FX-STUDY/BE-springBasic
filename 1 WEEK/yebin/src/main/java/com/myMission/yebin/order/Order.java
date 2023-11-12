08b9b1605bb0a5009b82325d0204b98814409bdc
02yebin <agabin0214@naver.com>
2023-11-12 13:59:28 +0900

    [BE] (REFACTOR) Oder 수정
    회원이 상품을 주문할때 사용하는 메서드입니다. 회원 Id와 상품 Name을 받아와 등급과 가격을 찾아내고 그에 해당하는 가격으로 주문을 했다고 표시합니다.

--- 1 WEEK/yebin/src/main/java/com/myMission/yebin/order/Order.java
+++ 1 WEEK/yebin/src/main/java/com/myMission/yebin/order/Order.java
@@ -2,20 +2,24 @@ package com.myMission.yebin.order;
 
 import com.myMission.yebin.grade.GradeVO;
 import com.myMission.yebin.item.ItemDAO;
+import com.myMission.yebin.item.ItemMap;
 import com.myMission.yebin.users.UserDAO;
 import com.myMission.yebin.users.UserVO;
 
+import java.util.Map;
+
 public class Order {
 
-    public void ItemOrder(String userName, String itemName){
+    private Map<String, Integer> itemMap = ItemMap.getInstance();
+    public void ItemOrder(String userId, String itemName){
         UserDAO userDAO = new UserDAO();
         ItemDAO itemDAO = new ItemDAO();
-        DiscountPolicy discountPolicy = new DiscountPolicy();
-//        discountPolicy.setDiscountByGrade();
-//        int itemPrice = itemDAO.findItemPriceByItemName(itemName) *;
+        DiscountPolicy discount = new DiscountPolicy();
+        Integer itemPrice = itemDAO.findItemPriceByItemName(itemName)
+                                - discount.setDiscountByGrade(userDAO.findGradeByUserId(userId));
 
-
+        System.out.println(userId + "님이 "+itemName+"을 주문했습니다. 가격 : " +itemPrice);
 
     }
-    
+
 }
