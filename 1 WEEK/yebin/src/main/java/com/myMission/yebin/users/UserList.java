a3e5878969dabe8a07b282a0ecf5c17fda0dcf02
02yebin <agabin0214@naver.com>
2023-11-12 13:53:10 +0900

    [BE] (FEAT) UserList 생성
    singleton 패턴을 사용하기위해 getInstance 메서드 생성

---
+++ 1 WEEK/yebin/src/main/java/com/myMission/yebin/users/UserList.java
@@ -0,0 +1,15 @@
+package com.myMission.yebin.users;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserList {
+    private static List<UserVO> userList;
+
+    public static List<UserVO> getInstance(){
+        if(userList == null){
+            userList = new ArrayList<>();
+        }
+        return userList;
+    }
+}
