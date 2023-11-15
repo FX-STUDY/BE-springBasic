d31b7492a5f05f84ce0451024d574ab377964e3e
02yebin <agabin0214@naver.com>
2023-11-15 18:46:47 +0900

    [BE] (REFACTOR) Order 클래스 수정
    ItemOrder() 메서드를 호출할때마다 객체를 생성해야해서 바깥에 빼주었고, 할인 정책 구분을 위한 인자도 추가해주었습니다.

--- 2 WEEK/SON/yebin/src/main/java/com/myMission/yebin/order/Order.java
+++ 2 WEEK/SON/yebin/src/main/java/com/myMission/yebin/order/Order.java
@@ -9,16 +9,16 @@ import com.myMission.yebin.users.UserVO;
 import java.util.Map;
 
 public class Order {
-
+    UserDAO userDAO;
+    DiscountPolicy discount;
     Map<String, Integer> itemMap = ItemMap.getInstance();
     // 상품 주문
     public void ItemOrder(String userId, String itemName){
-        UserDAO userDAO = new UserDAO();
-        DiscountPolicy discount = new DiscountPolicy();
+
         Integer findItemPrice = itemMap.get(itemName);
         GradeVO userGrade = userDAO.findGradeByUserId(userId);
 
-        Integer calculatedItemPrice = discount.calculateItemPrice(findItemPrice, userGrade);
+        Integer calculatedItemPrice = discount.calculateItemPrice(findItemPrice, userGrade, DiscountHow.RATE);
 
         System.out.println(userId + "님이 "+itemName+"을 주문했습니다. 가격 : " +calculatedItemPrice);
 
