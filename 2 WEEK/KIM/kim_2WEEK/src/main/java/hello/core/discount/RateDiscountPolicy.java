925758cd72826b2688880799616cad49165359c9
haramm <100928807+haramm@users.noreply.github.com>
2023-11-15 23:07:24 +0900

    [BE] (feat) RateDiscountPolicy 클래스 구현
    DiscountPolicy 인터페이스 구현하여 정률로 할인하는 기능 구현, 이 때 금액을 %로 할인하였을때 금액의 실수화를 막기 위해 int로 강제 형변환

---
+++ 2 WEEK/KIM/kim_2WEEK/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+//DiscountPolicy 구현하여 정률 할인정책 구현
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private final float discountRate = 0.1f;
+    @Override
+    public int discount(Member member, int price) {
+
+        if(member.getGrade() == Grade.VIP) {
+            return (int)(price * discountRate);
+        }
+        return price;
+    }
+}
