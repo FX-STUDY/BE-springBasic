4b8aa812e17a939eeab9a2485213572b5cd1061f
haramm <100928807+haramm@users.noreply.github.com>
2023-11-15 18:40:13 +0900

    [BE] (feat) ItemVO 구현
    장바구니에 담을 아이템의 정보를 담은 클래스를 생성하였습니다.

---
+++ 2 WEEK/KIM/kim_2WEEK/src/main/java/com/example/kim_2WEEK/item/ItemVO.java
@@ -0,0 +1,31 @@
+package com.example.kim_2WEEK.item;
+
+public class ItemVO {
+    private String itemName;
+    private int itemPrice;
+    private int orderNum;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(int orderNum) {
+        this.orderNum = orderNum;
+    }
+}
