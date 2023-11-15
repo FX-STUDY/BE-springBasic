78f0e04230f29735942cd2f37300ab0a83ccbdea
haramm <100928807+haramm@users.noreply.github.com>
2023-11-15 18:36:25 +0900

    [BE] (feat) userVO 구현
    user의 정보를 담을 클래스 생성

--- 2 WEEK/KIM/kim_2WEEK/src/main/java/com/example/kim_2WEEK/user/UserVO.java
+++ 2 WEEK/KIM/kim_2WEEK/src/main/java/com/example/kim_2WEEK/user/UserVO.java
@@ -1,4 +1,31 @@
 package com.example.kim_2WEEK.user;
 
 public class UserVO {
+    private int id;
+    private String name;
+    private String grade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
 }
