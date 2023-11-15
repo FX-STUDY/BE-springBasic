23cb538f6e2dae247981cc3c0c58c1a31d646771
SEOB <svj7531@naver.com>
2023-11-16 00:25:29 +0900

    [BE] (refactor) Main class 수정
    [BE] (refactor) Main class 수정
    
    오타 수정 및 코드 리팩토링

--- 2 WEEK/LEE/src/main/java/SEOB/SEOB/service/Main.java
+++ 2 WEEK/LEE/src/main/java/SEOB/SEOB/service/Main.java
@@ -7,7 +7,7 @@ import SEOB.SEOB.domain.Order;
 
 public class Main {
     public static void main(String[] args) {
-        MemberServiceImpl memberSerive = new MemberServiceImpl();
+        MemberServiceImpl memberService = new MemberServiceImpl();
 
         //회원 name, grade 지정
         Member member1 = new Member("name1", GradeType.VIP);
@@ -15,32 +15,29 @@ public class Main {
 
 
         //회원가입
-        Long id1 =  memberSerive.signUp(member1);
-        Long id2 = memberSerive.signUp(member2);
+        Long id1 =  memberService.signUp(member1);
+        Long id2 = memberService.signUp(member2);
         System.out.println(id1);
         System.out.println(id2);
 
 
         //id 검색 -> 앞서 지정한 name 출력
-        Member findMember1 = memberSerive.findByName(id1);
-        Member findMember2 = memberSerive.findByName(id2);
+        Member findMember1 = memberService.findByName(id1);
+        Member findMember2 = memberService.findByName(id2);
         System.out.println(findMember1.getName());
         System.out.println(findMember2.getName());
 
 
         //상품 주문
-        //고객의 고유 id, 상품
-        ProductService productService = new ProductService();
-        Order order1 = new Order("상품1", 10000L);//상품 이름과 가격
-        Order order2 = new Order("상품2", 20000L);
-        productService.order(id1, order1);
-        productService.order(id1, order1); //key값을 member의 id로, value에 product 주입
-        productService.order(id2, order2);
+        //고객의 고유 id, 상품, 가격
+        OrderService orderService = new OrderServiceImpl();
+        Order order1 = orderService.createOrder(member1.getId(), "itemA", 10000); //id1
+        Order order2 = orderService.createOrder(member2.getId(), "itemB", 20000); //id2
 
-        //등급별 할인된 가격 확인
-        System.out.println(order1.getDiscountedPrice());
-        System.out.println(order2.getDiscountedPrice());
 
+        //등급별 할인 가격 확인
+        System.out.println(order1.getDiscountedPrice()); // 1000
+        System.out.println(order2.getDiscountedPrice()); // 0
 
 
 
