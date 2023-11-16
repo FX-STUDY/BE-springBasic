75515eb3be664300372c587d1e6b3cf7be16f578
02yebin <agabin0214@naver.com>
2023-11-16 14:17:28 +0900

    [ BE ] (perf) OrderService 인터페이스 수정
    [ BE ] (perf) OrderService 인터페이스 수정
    
    Order 클래스에 있던 calculatePrice() 메서드를 빼와 서비스에 넣어주었습니다.

--- 2 WEEK/SON/yebin/src/main/java/com/yebin/yebin/order/OrderService.java
+++ 2 WEEK/SON/yebin/src/main/java/com/yebin/yebin/order/OrderService.java
@@ -2,4 +2,5 @@ package com.yebin.yebin.order;
 
 public interface OrderService {
     Order createOrder(Long memberId, String itemName, int itemPrice);
+    float calculatePrice(int itemPrice, float discountPrice);
 }
