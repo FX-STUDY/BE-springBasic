e97d94694cc3fef89a17ea7af13df37ae6a6a924
02yebin <agabin0214@naver.com>
2023-11-16 13:01:51 +0900

    [ BE ] (pref) OrderService 클래스의 할인 정책 변경
    [ BE ] (pref) OrderService 클래스의 할인 정책 변경
    
    고정 금액 할인에서 정률 할인으로 바뀌었으므로 적용하는 메서드 또한, 바꾸어주었습니다.

--- 2 WEEK/SON/yebin/src/main/java/com/yebin/yebin/order/OrderServiceImpl.java
+++ 2 WEEK/SON/yebin/src/main/java/com/yebin/yebin/order/OrderServiceImpl.java
@@ -2,13 +2,14 @@ package com.yebin.yebin.order;
 
 import com.yebin.yebin.discount.DiscountPolicy;
 import com.yebin.yebin.discount.FixDiscountPolicy;
+import com.yebin.yebin.discount.RateDiscountPolicy;
 import com.yebin.yebin.member.Member;
 import com.yebin.yebin.member.MemberRepository;
 import com.yebin.yebin.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
