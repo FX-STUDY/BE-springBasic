f4c1313b7f3f02aa7a45f90e386a50c957fc8d42
haramm <100928807+haramm@users.noreply.github.com>
2023-11-20 15:37:40 +0900

    [BE] (refactor) Lombok 사용에 따른 코드 변경
    @Getter, @Setter 대신 사용하여 코드 간결화

--- 2 WEEK/KIM/kim_2WEEK/src/main/java/hello/core/member/Member.java
+++ 2 WEEK/KIM/kim_2WEEK/src/main/java/hello/core/member/Member.java
@@ -1,5 +1,10 @@
 package hello.core.member;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Member {
 
     private Long id;
@@ -11,27 +16,4 @@ public class Member {
         this.grade = grade;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Grade getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Grade grade) {
-        this.grade = grade;
-    }
 }
--- 2 WEEK/KIM/kim_2WEEK/src/main/java/hello/core/order/Order.java
+++ 2 WEEK/KIM/kim_2WEEK/src/main/java/hello/core/order/Order.java
@@ -1,38 +1,15 @@
 package hello.core.order;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Order {
     private Long memberId;
     private String itemName;
     private int itemPrice;
     private int discountPrice;
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-
-    public int getItemPrice() {
-        return itemPrice;
-    }
-
-    public void setItemPrice(int itemPrice) {
-        this.itemPrice = itemPrice;
-    }
-
-    public void setDiscountPrice(int discountPrice) {
-        this.discountPrice = discountPrice;
-    }
 
     public Order(Long memberId, String itemName, int itemPrice, int discountPrice){
         this.memberId = memberId;
