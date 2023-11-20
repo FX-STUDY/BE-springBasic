9c827c83046e54b3578f54bac56e8a5a35a512f9
02yebin <agabin0214@naver.com>
2023-11-20 16:10:50 +0900

    [ BE ] (perf) DependencyInjection 클래스 명 수정
    [ BE ] (perf) DependencyInjection 클래스 명 수정
    
    DependencyInjection 클래스명을 AppConfig로 수정하였습니다.

--- 2 WEEK/SON/yebin/src/main/java/com/yebin/yebin/dependency/AppConfig.java
+++ 2 WEEK/SON/yebin/src/main/java/com/yebin/yebin/dependency/AppConfig.java
@@ -3,9 +3,15 @@ package com.yebin.yebin.dependency;
 import com.yebin.yebin.discount.DiscountPolicy;
 import com.yebin.yebin.discount.RateDiscountPolicy;
 import com.yebin.yebin.member.MemberRepository;
+import com.yebin.yebin.member.MemberService;
+import com.yebin.yebin.member.MemberServiceImpl;
 import com.yebin.yebin.member.MemoryMemberRepository;
+import com.yebin.yebin.order.OrderService;
+import com.yebin.yebin.order.OrderServiceImpl;
+import org.springframework.context.annotation.Configuration;
 
-public class DependencyInjection {
+@Configuration
+public class AppConfig {
     public DiscountPolicy discountPolicy(){
         // 할인 정책이 바뀌면 이 부분만 바꿔주면 된다.
         return new RateDiscountPolicy();
@@ -14,5 +20,13 @@ public class DependencyInjection {
     public MemberRepository memberRepository(){
         // DB 연결하면 이 부분만 바꿔주면 된다.
         return new MemoryMemberRepository();
+    }
+
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 }
