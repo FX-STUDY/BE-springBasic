72c80c238deb987fcb3d0de947839608b99b5119
02yebin <agabin0214@naver.com>
2023-11-20 22:51:00 +0900

    [ BE ] (perf) MemoryMemberRepository 클래스 수정
    [ BE ] (perf) MemoryMemberRepository 클래스 수정
    
    빈 등록해주었습니다.

--- 2 WEEK/SON/yebin/src/main/java/com/yebin/yebin/member/MemoryMemberRepository.java
+++ 2 WEEK/SON/yebin/src/main/java/com/yebin/yebin/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package com.yebin.yebin.member;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long,Member> store = new HashMap<>();
     private static long SEQUENCE = 0L;
