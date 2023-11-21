9a3983e304dfe0f8951b178445d398567ee80774
JIUNG YANG <yangjiung286@gmail.com>
2023-11-21 16:31:53 +0900

    [ BE ] (feat) Refactor OrderApp Class
    [ BE ] (feat) Refactor OrderApp Class
    
    컨테이너에서 Bean을 가져와 사용했습니다.

---
+++ 3 WEEK/core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,28 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class OrderApp {
+    public static void main(String[] args) {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+
+        Member member = new Member("memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(member.getId(),"itemA",10000);
+
+        System.out.println("order = " + order);
+
+    }
+}
