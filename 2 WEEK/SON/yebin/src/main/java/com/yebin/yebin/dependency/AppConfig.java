03ae4c70088955331387798eacf9c3d0d29487da
02yebin <agabin0214@naver.com>
2023-11-21 21:48:30 +0900

    [ BE ] (perf) AppConfig 클래스 수정
    [ BE ] (perf) AppConfig 클래스 수정
    
    빈을 수동으로 등록하도록 어노테이션 Bean 을 사용하였습니다.

--- 2 WEEK/SON/yebin/src/main/java/com/yebin/yebin/dependency/AppConfig.java
+++ 2 WEEK/SON/yebin/src/main/java/com/yebin/yebin/dependency/AppConfig.java
@@ -8,24 +8,29 @@ import com.yebin.yebin.member.MemberServiceImpl;
 import com.yebin.yebin.member.MemoryMemberRepository;
 import com.yebin.yebin.order.OrderService;
 import com.yebin.yebin.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    @Bean
     public DiscountPolicy discountPolicy(){
         // 할인 정책이 바뀌면 이 부분만 바꿔주면 된다.
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public MemberRepository memberRepository(){
         // DB 연결하면 이 부분만 바꿔주면 된다.
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
