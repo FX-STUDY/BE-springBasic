cc05771cdc9b3c6199e122e5a160f2bdafbf5e08
JIUNG YANG <yangjiung286@gmail.com>
2023-11-21 16:29:38 +0900

    [ BE ] (feat) Refactor MemberApp Class
    [ BE ] (feat) Refactor MemberApp Class
    
    컨테이너에서 Bean을 가져와 사용했습니다.

---
+++ 3 WEEK/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,30 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        Member member = new Member("memberA", Grade.VIP);
+
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+
+
+    }
+
+}
