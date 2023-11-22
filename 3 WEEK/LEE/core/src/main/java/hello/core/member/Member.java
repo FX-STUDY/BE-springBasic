f204224b555835a542ab80f0362671b65c852bbb
SEOB <svj7531@naver.com>
2023-11-22 22:05:42 +0900

    [BE] (chore) Member 클래스 수정
    [BE] (chore) Member 클래스 수정
    
    기존 Getter Setter 에서 스프링 어노테이션을 사용한 Getter Setter 로 변경

--- 3 WEEK/LEE/core/src/main/java/hello/core/member/Member.java
+++ 3 WEEK/LEE/core/src/main/java/hello/core/member/Member.java
@@ -1,5 +1,10 @@
 package hello.core.member;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Member {
 
     private Long id;
@@ -8,30 +13,6 @@ public class Member {
 
     public Member( String name, Grade grade) {
         this.name = name;
-        this.grade = grade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Grade getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Grade grade) {
         this.grade = grade;
     }
 }
