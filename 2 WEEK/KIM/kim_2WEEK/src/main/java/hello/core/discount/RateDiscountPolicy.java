15f5c4bca607c3f58f28da886165dd4b76f3bda6
haramm <100928807+haramm@users.noreply.github.com>
2023-11-23 16:26:38 +0900

    [BE] (refactor) RateDiscountPolicy 기능 수정
    등급이 VIP가 아닐때 할인가격 0 이아닌 원래 가격 price가 리턴되는 문제 수정, 스프링 어노테이션 사용을 위한 @Component 활용

--- 2 WEEK/KIM/kim_2WEEK/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ 2 WEEK/KIM/kim_2WEEK/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,17 +2,27 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.springframework.stereotype.Component;
 
 //DiscountPolicy 구현하여 정률 할인정책 구현
+@Component("rateDiscountPolicy")
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private final int discountRate = 10;
     @Override
-    public int discount(Member member, int price) {
+    public int discount(long memberID, int price) {
+
+        MemberService memberService = new MemberServiceImpl();
+
+        Member member = memberService.findMember(memberID);
+
 
         if(member.getGrade() == Grade.VIP) {
-            return (int)((100 - discountRate) * 0.01 * price);
+            int discountAmount = (int)(discountRate * 0.01 * price);
+            return discountAmount;
         }
-        return price;
+        return 0;
     }
 }
