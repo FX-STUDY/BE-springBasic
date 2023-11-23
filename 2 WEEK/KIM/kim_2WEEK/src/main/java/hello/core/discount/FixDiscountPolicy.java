bbdd48c76193afa276b56963fe69d0a22a594f6f
haramm <100928807+haramm@users.noreply.github.com>
2023-11-23 16:27:35 +0900

    [BE] (refactor) FixDiscountPolicy 수정
    스프링 어노테이션 활용하기 위한 @Component 추가

--- 2 WEEK/KIM/kim_2WEEK/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ 2 WEEK/KIM/kim_2WEEK/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,13 +2,21 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.springframework.stereotype.Component;
 
+@Component("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;
 
     @Override
-    public int discount(Member member, int price) {
+    public int discount(long memberID, int price) {
+
+        MemberService memberService = new MemberServiceImpl();
+
+        Member member = memberService.findMember(memberID);
 
         if (member.getGrade() == Grade.VIP) {
             return discountFixAmount;
