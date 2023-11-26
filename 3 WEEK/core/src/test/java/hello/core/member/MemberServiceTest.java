1dc18cac2f3295e50e640003eaa4b76cb77e8c09
shinyeongwoon <duddns119@naver.com>
2023-11-26 09:26:38 +0900

    [ feat ] 3 week answer Update
    [ feat ] 3 week answer Update

---
+++ 3 WEEK/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,34 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member("memberA",Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        assertThat(member).isEqualTo(findMember);
+
+    }
+}
