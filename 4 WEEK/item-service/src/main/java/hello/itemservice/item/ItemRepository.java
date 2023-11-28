6fdb74291560c229516495df3db6d0641063bfb8
02yebin <agabin0214@naver.com>
2023-11-28 10:59:29 +0900

    [ BE ] (perf) ItemPepository 어노테이션 추가
    [ BE ] (perf) ItemPepository 어노테이션 추가
    
    자동 등록 사용을 위한 어노테이션 Repository를 추가하였습니다.

--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemRepository.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemRepository.java
@@ -1,5 +1,8 @@
 package hello.itemservice.item;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ItemRepository {
     void save(Item item);
     Item findById(Long itemId);
