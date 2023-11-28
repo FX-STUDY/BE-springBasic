a3f7c3fb8f4d632eed76f1af5ab2f32e49dae8b1
02yebin <agabin0214@naver.com>
2023-11-28 11:21:56 +0900

    [ BE ] (perf) ItemService 어노테이션 추가
    [ BE ] (perf) ItemService 어노테이션 추가
    
    자동 등록을 위한 어노테이션 Service를 추가해주었습니다.

--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemService.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemService.java
@@ -1,5 +1,8 @@
 package hello.itemservice.item;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface ItemService {
     void join(Item item);
     void updateItem(Item item);
