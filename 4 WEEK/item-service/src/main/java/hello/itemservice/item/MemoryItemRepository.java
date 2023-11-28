735c6c536307dae94b9195aacc9edd78a3514480
02yebin <agabin0214@naver.com>
2023-11-28 11:07:37 +0900

    [ BE ] (feat) MemoryItemRepository 클래스 수정
    [ BE ] (feat) MemoryItemRepository 클래스 수정
    
    save() 메서드를 오버라이딩하여 상품을 저장하는 메서드로 만들어주었습니다.

--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/MemoryItemRepository.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/MemoryItemRepository.java
@@ -6,4 +6,10 @@ import java.util.Map;
 public class MemoryItemRepository implements ItemRepository {
     private static Map<Long, Item> store = new HashMap<>();
     private static long SEQUENCE = 0L;
+    @Override
+    public void save(Item item) {
+        item.setId(++SEQUENCE);
+        store.put(item.getId(),item);
+    }
+
 }
