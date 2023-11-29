3cfb5cc1d290453d2e66c34dd761549968931be5
02yebin <agabin0214@naver.com>
2023-11-30 00:50:40 +0900

    [ BE ] (perf) ItemService 클래스 수정
    [ BE ] (perf) ItemService 클래스 수정
    
    상품 수정에 필요한 매개변수를 서비스 쪽에서 받아오도록 했습니다.

--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemService.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public interface ItemService {
     void join(Item item);
-    void updateItem(Item item);
+    void updateItem(Item item, String itemName, double itemPrice, long itemQuantity);
     Item findItem(Long itemId);
 }
