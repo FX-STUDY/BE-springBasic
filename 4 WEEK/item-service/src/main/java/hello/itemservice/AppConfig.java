da807c68410928430a677296519465cd4b1b9b78
SEOB <svj7531@naver.com>
2023-12-01 00:23:40 +0900

    [BE] (feat) AppConfig 생성
    [BE] (feat) AppConfig 생성

---
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/AppConfig.java
@@ -0,0 +1,24 @@
+package hello.itemservice;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.MemoryItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ItemService itemService(){
+        return new ItemServiceImpl(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository(){
+        return new MemoryItemRepository();
+    }
+
+}
+
