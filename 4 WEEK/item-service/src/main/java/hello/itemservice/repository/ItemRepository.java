50195fd7c310fdd30a106d51b745096457e8285b
SEOB <svj7531@naver.com>
2023-12-01 00:22:58 +0900

    [BE] (chore) public 제거
    [BE] (chore) public 제거

--- 4 WEEK/item-service/src/main/java/hello/itemservice/repository/ItemRepository.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/repository/ItemRepository.java
@@ -4,6 +4,7 @@ import hello.itemservice.domain.Item;
 
 public interface ItemRepository {
 
-    public void save(Item item);
-    public Item findByItemId(Long itemId);
+    void save(Item item);
+
+    Item findByItemId(Long itemId);
 }
--- 4 WEEK/item-service/src/main/java/hello/itemservice/service/ItemService.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/service/ItemService.java
@@ -5,8 +5,8 @@ import hello.itemservice.domain.Item;
 public interface ItemService {
 
     //상품 목록 추가
-    public void add(Item item);
+    void add(Item item);
 
     //상품상세
-    public Item findItem(Long itemId);
+    Item findItem(Long itemId);
 }
