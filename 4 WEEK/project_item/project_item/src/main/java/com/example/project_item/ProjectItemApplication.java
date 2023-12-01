c32e532f51e20c3d5ca57e951107d0a13f524da6
haramm <100928807+haramm@users.noreply.github.com>
2023-12-01 13:37:58 +0900

    [BE] (feat) 초기설정
    스프링 스타터 사용하여 초기 프로젝트 생성

---
+++ 4 WEEK/project_item/project_item/src/main/java/com/example/project_item/ProjectItemApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectItemApplication.class, args);
+	}
+
+}
---
+++ 4 WEEK/project_item/project_item/src/test/java/com/example/project_item/ProjectItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
