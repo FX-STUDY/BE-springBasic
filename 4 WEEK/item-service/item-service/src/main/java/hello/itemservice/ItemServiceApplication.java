81015c126e59b4212a44dafb87955b18288ee5e6
haramm <100928807+haramm@users.noreply.github.com>
2023-12-15 14:35:55 +0900

    [BE] (feat) 초기 재설정
    artifact, project metadata group 이름 설정오류로 삭제 후 다시 초기설정함.

--- 4 WEEK/item-service/item-service/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ 4 WEEK/item-service/item-service/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.project_item;
+package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectItemApplication {
+public class ItemServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectItemApplication.class, args);
+		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
 }
--- 4 WEEK/item-service/item-service/src/test/java/hello/itemservice/ItemServiceApplicationTests.java
+++ 4 WEEK/item-service/item-service/src/test/java/hello/itemservice/ItemServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.project_item;
+package hello.itemservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectItemApplicationTests {
+class ItemServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
