3c69c3daf9fc30a5f845b9fffd297647376715aa
02yebin <agabin0214@naver.com>
2023-12-15 14:31:14 +0900

    [ BE ] (perf) ItemService 클래스 어노테이션 수정
    [ BE ] (perf) ItemService 클래스 어노테이션 수정
    
    @Service 가 인터페이스에 설정되어있어 구현체로 수정해주었습니다.

--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemService.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemService.java
@@ -2,7 +2,7 @@ package hello.itemservice.item;
 
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface ItemService {
     void join(Item item);
     void updateItem(Item item, String itemName, double itemPrice, long itemQuantity);
--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemServiceImpl.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemServiceImpl.java
@@ -1,7 +1,9 @@
 package hello.itemservice.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ItemServiceImpl implements ItemService{
     private final ItemRepository itemRepository;
 
