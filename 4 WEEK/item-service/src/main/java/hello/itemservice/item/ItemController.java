b7f3070c36f3c086b7d3ee20cf37b6f94d593f8f
02yebin <agabin0214@naver.com>
2023-12-17 01:17:21 +0900

    [ BE ] (perf) ItemController 수정
    [ BE ] (perf) ItemController 수정
    
    상품 상세폼에 ItemRepository가 필요해 선언해주었습니다.

--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemController.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemController.java
@@ -1,10 +1,21 @@
 package hello.itemservice.item;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
 
     @GetMapping("/itemList")
     public String getItemListForm(){
