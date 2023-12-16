1e0968e628fefc4391ac17902263d68c03a8a868
02yebin <agabin0214@naver.com>
2023-12-17 01:29:41 +0900

    [ BE ] (feat) Repsitory 인터페이스에 findAll() 메서드 추가
    [ BE ] (feat) Repsitory 인터페이스에 findAll() 메서드 추가
    
    @Override 해주지않으니 findAll() 메서드를 찾지 못해 인터페이스에도 추가해주었습니다.

--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemRepository.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemRepository.java
@@ -2,9 +2,12 @@ package hello.itemservice.item;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 public interface ItemRepository {
     void save(Item item);
     Item findById(Long itemId);
     void update(long itemId, Item updatedItem);
+    List<Item> findAll();
 }
