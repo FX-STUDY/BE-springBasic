8a801075e5a16730b6c5d7ccdf4788c91eef4c8f
SEOB <svj7531@naver.com>
2023-12-17 15:53:28 +0900

    [BE] (feat) 상품상세 Controller
    [BE] (feat) 상품상세 Controller

--- 4 WEEK/item-service/src/main/java/hello/itemservice/controller/ItemController.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/controller/ItemController.java
@@ -2,19 +2,24 @@ package hello.itemservice.controller;
 
 
 import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ItemController {
 
     private ItemService itemService;
+    private ItemRepository itemRepository;
     @Autowired
-    public ItemController(ItemService itemService) {
+    public ItemController(ItemService itemService, ItemRepository itemRepository) {
         this.itemService = itemService;
+        this.itemRepository = itemRepository;
     }
 
     @GetMapping("/addItem")
@@ -34,5 +39,13 @@ public class ItemController {
         itemService.addItem(item);
 
         return "redirect:/";
+    }
+
+    //상품상세
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findByItemId(itemId);
+        model.addAttribute("item", item);
+        return "/itemDetailForm";
     }
 }
