0091f2c0fded7cf420d7dea6873af9513c92571a
02yebin <agabin0214@naver.com>
2023-12-20 23:50:22 +0900

    [ BE ] (perf) Controller 클래스 requestMapping 추가
    [ BE ] (perf) Controller 클래스 requestMapping 추가
    
    클래스 단위로 @RequestMapping 을 해주었습니다.

--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemController.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemController.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/item")
 public class ItemController {
 
     private final ItemRepository itemRepository;
