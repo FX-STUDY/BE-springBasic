7c8959fcb7130699ca54203d1b5e456d2e68e042
02yebin <agabin0214@naver.com>
2023-12-23 17:07:07 +0900

    [ BE ] (perf) Controller 수정
    [ BE ] (perf) Controller 수정
    
    매핑 주소를 수정하고 url로 정보를 넘길 필요 없는 메서드는 post 매핑으로 수정해주었습니다.

--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemController.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemController.java
@@ -1,11 +1,11 @@
 package hello.itemservice.item;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,36 +28,36 @@ public class ItemController {
         return "ItemListForm";
     }
 
-    @GetMapping("/itemDetailForm/{itemId}")
+    @GetMapping("/{itemId}")
     public String getItemDetailForm(@PathVariable Long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item",item);
         return "ItemDetailForm";
     }
 
-    @GetMapping("/itemSaveForm")
+    @GetMapping("/itemSave")
     public String getItemSaveForm(){
         return "ItemSaveForm";
     }
 
-    @GetMapping("/itemSave")
     public String itemSave(Item item, Model model){
+    @PostMapping("/itemSave")
         itemRepository.save(item);
         List<Item> itemList = itemRepository.findAll();
         model.addAttribute("itemList",itemList);
         return "ItemListForm";
     }
 
-    @GetMapping("/itemUpdateForm")
     public String getItemUpdateForm(Item item, Model model){
         Item updateItem = itemRepository.findById(item.getId());
         model.addAttribute("updateItem",updateItem);
+    @GetMapping("/{itemId}/itemUpdate")
         return "ItemUpdateForm";
     }
 
-    @GetMapping("/itemUpdate")
     public String itemUpdate(Item item, Model model){
         itemRepository.update(item);
+    @PostMapping("/{itemId}/itemUpdate")
         Item updatedItem = itemRepository.findById(item.getId());
         model.addAttribute("item",updatedItem);
         return "ItemDetailForm";
