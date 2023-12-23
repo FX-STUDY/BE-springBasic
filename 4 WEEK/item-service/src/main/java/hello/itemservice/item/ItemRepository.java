43ce8b19180af03373438e645200039caad8bf2a
02yebin <agabin0214@naver.com>
2023-12-23 16:59:05 +0900

    [ BE ] (perf) Repository 인터페이스 수정
    [ BE ] (perf) Repository 인터페이스 수정
    
    테스트를 위해 Item 객체를 반환하도록 수정하였습니다.

--- 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemRepository.java
+++ 4 WEEK/item-service/src/main/java/hello/itemservice/item/ItemRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 
 public interface ItemRepository {
-    void save(Item item);
+    Item save(Item item);
     Item findById(Long itemId);
     void update(Long itemId, Item updatedItem);
     List<Item> findAll();
