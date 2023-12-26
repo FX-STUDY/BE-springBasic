6225cbac8e4634abad3609324ef4dad3a526a765
SEOB <67796853+seob7@users.noreply.github.com>
2023-12-26 21:19:31 +0900

    [BE] (refactor) Refactor NetworkClient lifecycle methods
    [BE] (refactor) Refactor NetworkClient lifecycle methods
    
    스프링 빈을 사용하기 위한 메서드 수정

--- 7 WEEK/seob/src/test/java/week7/seob/lifecycle/NetworkClient.java
+++ 7 WEEK/seob/src/test/java/week7/seob/lifecycle/NetworkClient.java
@@ -1,16 +1,12 @@
 package week7.seob.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -32,17 +28,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     //의존관계 주입이 끝난 후
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("\nNetworkClient.afterPropertiesSet");
+    public void init() {
+        System.out.println("\nNetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
     //Bean이 종료될 때
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("\nNetworkClient.destory");
+    public void close() {
+        System.out.println("\nNetworkClient.close");
         disconnect();
     }
 }
