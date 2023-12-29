878c2f9edadc080f9abbc51ed3fc7a6e061670e0
SEOB <67796853+seob7@users.noreply.github.com>
2023-12-29 16:58:43 +0900

    [BE] (refactor) Using ObjectProvider to SingletonWithPrototypeTest1.java
    [BE] (refactor) Using ObjectProvider to SingletonWithPrototypeTest1.java
    
    ObjectProvider를 사용하도록 변경

--- 7 WEEK/seob/src/test/java/week7/seob/scope/SingletonWithPrototypeTest1.java
+++ 7 WEEK/seob/src/test/java/week7/seob/scope/SingletonWithPrototypeTest1.java
@@ -4,8 +4,8 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -40,18 +40,12 @@ public class SingletonWithPrototypeTest1 {
 
     @Scope("singleton")
     static class ClientBean{
-//        private final PrototypeBean prototypeBean; //생성 시점에 주입
-//
-//        @Autowired
-//        public ClientBean(PrototypeBean prototypeBean) {
-//            this.prototypeBean = prototypeBean;
-//        }
 
         @Autowired
-        private ApplicationContext ac;
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
