44ebe57e93436e3a22a39727b70df0b75aa22c14
SEOB <67796853+seob7@users.noreply.github.com>
2023-12-29 16:10:20 +0900

    [BE] (refactor) Edit Client static class to SingletonWithProrototyleTest1.java
    [BE] (refactor) Edit Client static class to SingletonWithProrototyleTest1.java
    
    싱글톤 빈이 프로토타입 빈을 사용할 때 마다 스프링 컨테이너에 새로 요청하도록 변경

--- 7 WEEK/seob/src/test/java/week7/seob/scope/SingletonWithPrototypeTest1.java
+++ 7 WEEK/seob/src/test/java/week7/seob/scope/SingletonWithPrototypeTest1.java
@@ -5,6 +5,7 @@ import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -33,20 +34,24 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; //생성 시점에 주입
+//        private final PrototypeBean prototypeBean; //생성 시점에 주입
+//
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ApplicationContext ac;
 
         public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
