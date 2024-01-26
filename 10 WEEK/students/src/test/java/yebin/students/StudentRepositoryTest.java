ff326dfd32157f39f1a370453bd5928e43fc98ca
02yebin <agabin0214@naver.com>
2024-01-27 03:38:01 +0900

    [ BE ] (test) findAll() test
    [ BE ] (test) findAll() test
    
    findAll() 메서드를 테스트하여 정상 작동하였습니다.

--- 10 WEEK/students/src/test/java/yebin/students/StudentRepositoryTest.java
+++ 10 WEEK/students/src/test/java/yebin/students/StudentRepositoryTest.java
@@ -25,3 +25,17 @@ public class StudentRepositoryTest {
         Assertions.assertThat(findStudent).isEqualTo(saveStudent);
     }
 
+    @Test
+    void findAll(){
+        //given
+        Students student1 = new Students("yebin", 4, "소프트웨어학과");
+        Students student2 = new Students("son", 4, "소프트웨어학과");
+        studentRepository.save(student1);
+        studentRepository.save(student2);
+        //when
+        List<Students> students = studentRepository.findAll();
+        //then
+        Assertions.assertThat(students.size()).isEqualTo(2);
+        Assertions.assertThat(students).contains(student1,student2);
+    }
+
