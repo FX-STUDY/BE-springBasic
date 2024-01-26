4e3735c2e4b24ef1b6a574e7e944baf48571881a
02yebin <agabin0214@naver.com>
2024-01-27 03:44:25 +0900

    [ BE ] (feat) service impl 생성
    [ BE ] (feat) service impl 생성
    
    @Service, @RequiredArgsConstructor 어노테이션을 이용하여 repository를 등록시켜주었습니다.

---
+++ 10 WEEK/students/src/main/java/yebin/students/StudentServiceImpl.java
@@ -0,0 +1,12 @@
+package yebin.students;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentServiceImpl implements StudentService{
+    private final StudentRepository studentRepository;
+}
