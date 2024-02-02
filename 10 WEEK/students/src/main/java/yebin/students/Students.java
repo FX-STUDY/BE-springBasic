ee76db09c66d24f4c05426a949a6dd93f63878d0
02yebin <agabin0214@naver.com>
2024-02-02 11:28:30 +0900

    [ BE ] (perf) Students 클래스 수정
    [ BE ] (perf) Students 클래스 수정
    
    빌더패턴을 사용하여 @Data -> @Getter, @Setter로 수정하였습니다.
    또한, 학번은 사용자에게 입력받도록 생성자에 추가하였습니다.

--- 10 WEEK/students/src/main/java/yebin/students/Students.java
+++ 10 WEEK/students/src/main/java/yebin/students/Students.java
@@ -1,16 +1,20 @@
 package yebin.students;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class Students {
     private String name;
     private Long id;
     private Integer grade;
     private String major;
 
-    public Students(String name, Integer grade, String major){
+    public Students(String name, Long id,Integer grade, String major){
         this.name = name;
+        this.id = id;
         this.grade = grade;
         this.major = major;
     }
