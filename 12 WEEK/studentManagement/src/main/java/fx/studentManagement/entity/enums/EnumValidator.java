17d9f247267f54013aaf9f6c43cb7b2556c70893
SEOB <67796853+seob7@users.noreply.github.com>
2024-02-06 21:07:21 +0900

    [BE] (feat) enum Valid 구현
    ENUM 유효성 검증을 위해 아래 내용 참고하였습니다.
    https://jsy1110.github.io/2022/enum-class-validation/

---
+++ 12 WEEK/studentManagement/src/main/java/fx/studentManagement/entity/enums/EnumValidator.java
@@ -0,0 +1,31 @@
+package fx.studentManagement.entity.enums;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+/*
+*  https://jsy1110.github.io/2022/enum-class-validation/
+ */
+public class EnumValidator implements ConstraintValidator<ValidEnum, Enum> {
+    private ValidEnum annotation;
+
+    @Override
+    public void initialize(ValidEnum constraintAnnotation) {
+        this.annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(Enum value, ConstraintValidatorContext context) {
+        boolean result = false;
+        Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+        if (enumValues != null) {
+            for (Object enumValue : enumValues) {
+                if (value == enumValue) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
