43e892b7ad44eeb8a4b2e37530d271610094b2d8
SEOB <67796853+seob7@users.noreply.github.com>
2024-02-06 15:02:39 +0900

    [BE] (feat) 회원가입 및 정보수정 Form 구현

---
+++ 12 WEEK/studentManagement/src/main/java/fx/studentManagement/controller/form/EditStudentForm.java
@@ -0,0 +1,11 @@
+package fx.studentManagement.controller.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EditStudentForm {
+    private String name;
+    private String address;
+}
---
+++ 12 WEEK/studentManagement/src/main/java/fx/studentManagement/controller/form/SignUpForm.java
@@ -0,0 +1,20 @@
+package fx.studentManagement.controller.form;
+
+import fx.studentManagement.entity.enums.Major;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class SignUpForm {
+//학번, 이름, 생년월일, 학과, 학기, 주소, 학년
+    private Long studentNumber;
+    private String studentName;
+    private Date studentBirth; //yyyy-mm-dd
+    private Major studentMajor;
+    private int studentSemester;
+    private String studentAddress;
+    private int studentGrade;
+}
