5a0975c4b846a4827f3f44f5b45fab9e02a9f275
SEOB <67796853+seob7@users.noreply.github.com>
2024-02-07 14:34:41 +0900

    [BE] (fix) @Range 오류로 인한 주석처리

--- 12 WEEK/studentManagement/src/main/java/fx/studentManagement/controller/form/SignUpForm.java
+++ 12 WEEK/studentManagement/src/main/java/fx/studentManagement/controller/form/SignUpForm.java
@@ -13,7 +13,7 @@ public class SignUpForm {
 //학번, 이름, 생년월일, 학과, 학기, 주소, 학년
 
     //https://coderanch.com/t/586580/java/validate-length-number
-    @Range(min=7, max=7, message = "- 7자리, 숫자 ( 영문 , 특수 기호 금지 )")
+//    @Range(min=7, max=7, message = "- 7자리, 숫자 ( 영문 , 특수 기호 금지 )")
     private Long studentNumber;
 
     @Size(min =2, max = 7)
