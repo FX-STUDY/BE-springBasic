c53934d268b60ccda22d0eaa88543f23be46fd7d
SEOB <67796853+seob7@users.noreply.github.com>
2024-02-07 17:13:31 +0900

    [BE] (refactor) Controller 수정

--- 12 WEEK/studentManagement/src/main/java/fx/studentManagement/controller/StudentController.java
+++ 12 WEEK/studentManagement/src/main/java/fx/studentManagement/controller/StudentController.java
@@ -1,5 +1,6 @@
 package fx.studentManagement.controller;
 
+import fx.studentManagement.common.ResponseMessage;
 import fx.studentManagement.controller.form.EditStudentForm;
 import fx.studentManagement.controller.form.SignUpForm;
 import fx.studentManagement.entity.Student;
@@ -9,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -21,59 +23,45 @@ public class StudentController {
 
 
     @PostMapping //학생 등록
-    public ResponseEntity signUp(@Valid @RequestBody SignUpForm signUpForm) {
-        try {
-            studentService.signUp(signUpForm);
-            return new ResponseEntity<>("학생 등록 성공", HttpStatus.OK); // 학생 등록 확인을 위해 전체 출력
-        } catch (Exception e) {
-            return new ResponseEntity<>("이미 존재하는 학생입니다.", HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity signUp(@Valid @RequestBody SignUpForm signUpForm, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors())
+            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
+
+        studentService.signUp(signUpForm);
+        return new ResponseEntity<>(ResponseMessage.SUCCESS_REGISTERED_STUDENT.getMessage(), HttpStatus.OK); // 학생 등록 확인을 위해 전체 출력
     }
 
     @GetMapping("/{studentNumber}") // 단일 학생 조회
     public ResponseEntity showStudent(@PathVariable Long studentNumber) {
-        try {
-            return new ResponseEntity<>(studentService.showStudent(studentNumber), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>("존재하지 않는 학생입니다.", HttpStatus.BAD_REQUEST);
-        }
+        return new ResponseEntity<>(studentService.showStudent(studentNumber), HttpStatus.OK);
     }
 
     @GetMapping // 모든 학생 조회
     public ResponseEntity showAllStudent() {
-        try {
-            return new ResponseEntity<>(studentService.showAllStudent(), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>("존재하는 학생이 없습니다.", HttpStatus.BAD_REQUEST);
-        }
+        return new ResponseEntity<>(studentService.showAllStudent(), HttpStatus.OK);
     }
 
     @DeleteMapping("/{studentNumber}") // 단일 학생 삭제
     public ResponseEntity deleteStudent(@PathVariable Long studentNumber) {
-        try {
-            studentService.deleteStudent(studentNumber);
-            return new ResponseEntity<>("학번 : " + studentNumber + " 정보 삭제 성공", HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>("존재하지 않는 학생입니다.", HttpStatus.BAD_REQUEST);
-        }
+        studentService.deleteStudent(studentNumber);
+        return new ResponseEntity<>(studentNumber + ResponseMessage.SUCCESS_DELETE_STUDENT.getMessage(), HttpStatus.OK);
     }
 
     @DeleteMapping // 다중 학생 삭제
     public ResponseEntity deleteAllStudent() {
         studentService.deleteAllStudent();
-        return new ResponseEntity<>("학생 전체 정보가 삭제되었습니다.", HttpStatus.OK);
+        return new ResponseEntity<>(ResponseMessage.SUCCESS_DELETE_ALL_STUDENT.getMessage(), HttpStatus.OK);
     }
 
     @PatchMapping("/{studentNumber}") // 단일 학생 정보 수정
-    public ResponseEntity updateStudent(@PathVariable Long studentNumber, @RequestBody EditStudentForm editStudentForm) {
-        try{
-            Student updateStudent = studentService.updateStudent(studentNumber, editStudentForm);
-            return new ResponseEntity<>(updateStudent, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>("학생 정보 수정에 실패했습니다.", HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity editStudentInformation(@PathVariable Long studentNumber,@Valid @RequestBody EditStudentForm editStudentForm, BindingResult bindingResult) {
 
-    public ResponseEntity updateStudent(@PathVariable Long studentNumber,@Valid @RequestBody EditStudentForm editStudentForm) {
+        if(bindingResult.hasErrors())
+            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
+
+        Student updateStudent = studentService.editStudentInformation(studentNumber, editStudentForm);
+        return new ResponseEntity<>(updateStudent, HttpStatus.OK);
     }
 
     @GetMapping("/count") // 총 학생 조회
