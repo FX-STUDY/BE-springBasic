48a9fa0a381258e6586c22f7e3fad19d3f951d8a
02yebin <agabin0214@naver.com>
2024-02-13 04:01:43 +0900

    [ BE ] (feat) 제약 조건 클래스 생성
    [ BE ] (feat) 제약 조건 클래스 생성
    
    학번, 이름, 학과, 학기, 학년에 관한 제약 조건을 확인하는 메서드를 만들 예정입니다.

---
+++ 12 WEEK/student/student/src/main/java/student/student/validation/ValidateStudent.java
@@ -0,0 +1,11 @@
+package student.student.validation;
+
+import student.student.ENUM.Grade;
+import student.student.ENUM.Major;
+import student.student.ENUM.Semester;
+import student.student.Student;
+
+import java.util.regex.Pattern;
+
+public class ValidateStudent {
+}
