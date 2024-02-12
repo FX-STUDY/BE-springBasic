c63903faa7c90c5725817e7022697d6af673a710
02yebin <agabin0214@naver.com>
2024-02-13 04:02:45 +0900

    [ BE ] (feat) 학번 제약 조건 확인 메서드 생성
    [ BE ] (feat) 학번 제약 조건 확인 메서드 생성
    
    학번의 길이가 7인지, 숫자로 이루어져있는지 확인하는 메서드를 생성하였습니다.

--- 12 WEEK/student/student/src/main/java/student/student/validation/ValidateStudent.java
+++ 12 WEEK/student/student/src/main/java/student/student/validation/ValidateStudent.java
@@ -8,4 +8,15 @@ import student.student.Student;
 import java.util.regex.Pattern;
 
 public class ValidateStudent {
+    public boolean validateStudentId(Long studentId){
+        if(studentId.SIZE != 7){
+            return false;
+        }
+        if(!Pattern.matches("[0-9+]",String.valueOf(studentId))){
+            return false;
+        }
+
+        return true;
+    }
+
 }
