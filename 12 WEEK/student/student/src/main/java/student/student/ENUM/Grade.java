fd8d02d49fff11324287b85dba7bb77d9ba38e18
02yebin <agabin0214@naver.com>
2024-02-13 03:46:35 +0900

    [ BE ] (feat) 학생 정보에 관한 enum 클래스 생성
    [ BE ] (feat) 학생 정보에 관한 enum 클래스 생성
    
    학생 정보에 필요하면서 제한적인 정보들을 enum 클래스로 생성해주었습니다.

---
+++ 12 WEEK/student/student/src/main/java/student/student/ENUM/Grade.java
@@ -0,0 +1,5 @@
+package student.student.ENUM;
+
+public enum Grade {
+    freshman, sophomore, junior, senior, the_5th
+}
---
+++ 12 WEEK/student/student/src/main/java/student/student/ENUM/Major.java
@@ -0,0 +1,5 @@
+package student.student.ENUM;
+
+public enum Major {
+    소프트웨어학과, 컴퓨터공학과, 산업디자인학과
+}
