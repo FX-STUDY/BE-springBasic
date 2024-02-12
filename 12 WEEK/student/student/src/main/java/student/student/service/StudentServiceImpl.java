f7cd3d714aef4b76891d7f05b269012a84850c03
02yebin <agabin0214@naver.com>
2024-02-13 03:57:03 +0900

    [ BE] (feat) service saveStudent() 구현
    [ BE] (feat) service saveStudent() 구현
    
    회원가입을 진행할 때 어떠한 부분이 요구사항을 충족시키지 못했는지 알려주기위해 해당 정보를 반환하도록 만들었습니다.

--- 12 WEEK/student/student/src/main/java/student/student/service/StudentServiceImpl.java
+++ 12 WEEK/student/student/src/main/java/student/student/service/StudentServiceImpl.java
@@ -14,4 +14,35 @@ import java.util.Objects;
 public class StudentServiceImpl implements StudentService{
     private StudentRepository studentRepository;
     private ValidateStudent validateStudent;
+    @Override
+    public Object saveStudent(Student student) {
+        Student findStudent = studentRepository.findById(student.getId());
+        // 저장하려는 학생 정보가 이미 있다면 해당 학생 정보 반환
+        if (!Objects.isNull(findStudent)){
+            return findStudent;
+        }
+        // 저장하려는 학생의 학번이 요구사항을 충족하지 못했다면 학번 반환
+        if(!validateStudent.validateStudentId(student.getId())){
+            return student.getId();
+        }
+        // 저장하려는 학생의 이름이 요구사항을 충족하지 못했다면 이름 반환
+        if(!validateStudent.validateStudentName(student.getName())){
+            return student.getName();
+        }
+        // 저장하려는 학생의 학과가 요구사항을 충족하지 못했다면 학과 반환
+        if(!validateStudent.validateMajor(student.getMajor())){
+            return student.getMajor();
+        }
+        // 저장하려는 학생의 학기가 요구사항을 충족하지 못했다면 학기 반환
+        if(!validateStudent.validateSemester(student.getSemester())){
+            return student.getSemester();
+        }
+        // 저장하려는 학생의 학년이 요구사항을 충족하지 못했다면 학년 반환
+        if(!validateStudent.validateGrade(student.getGrade())){
+            return student.getGrade();
+        }
+
+        return studentRepository.save(student);
+    }
+
 }
